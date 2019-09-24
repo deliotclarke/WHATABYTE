195b09c89c31530182de4d79c2c27f470a06f1f7
Daniel Clarke <deliotclarke@gmail.com>
2019-09-24 11:35:51 -0500

    starting part 3

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 // pages/index.js
 
 import Layout from "../components/Layout";
+import Link from "next/link";
 
-const Index = () => <Layout>Welcome to WHATABYTE!</Layout>;
+const Index = () => {
+    <Layout>
+        <br />
+
+        <Link href="/explore">
+            <a>Welcome to WHATABYTE! Start Exploring Now!</a>
+        </Link>
+    </Layout>
+
+};
 
 export default Index;
