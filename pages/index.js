164d0a58648ce2315d8813678b0b146e8b533328
Daniel Clarke <deliotclarke@gmail.com>
2019-09-24 11:37:33 -0500

    added font awesome

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@
 import Layout from "../components/Layout";
 import Link from "next/link";
 
-const Index = () => {
+const Index = () => (
     <Layout>
         <br />
 
@@ -12,6 +12,6 @@ const Index = () => {
         </Link>
     </Layout>
 
-};
+);
 
 export default Index;
